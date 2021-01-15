860fa63ce5986825ff312fd34663bd771f1cc6aa
Lyly Long <lylylong@gmail.com>
2021-01-15 16:11:34 -0500

    add friend & delete friend routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -80,6 +80,40 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  // create a friend
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $addToSet: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete a user's friend
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,6 +6,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  deleteFriend,
 } = require("../../controllers/user-controller");
 
 // /api/users
@@ -13,5 +15,8 @@ router.route("/").get(getAllUsers).post(createUser);
 
 // /api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:id/friends/:friendId
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
